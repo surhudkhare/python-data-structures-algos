377712190393582a2aceecc1639c131e10e0dc90
Surhud <surhudk@gmail.com>
2024-07-19 16:20:37 -0700

    Simplify LinkedList

--- datastructures/linked_list.py
+++ datastructures/linked_list.py
@@ -1,228 +1,75 @@
 class Node:
-    def __init__(self, data):
-        """
-        A Node of a Singly-linked Linked List
-        """
+    def __init__(self, data) -> None:
         self.data = data
         self.next = None
 
-class LinkedList():
-    """A dynamic linear data structure of linked nodes where each node contains data and reference to the next node
-    It can be accessed by the first element 'head'
-    """
-    def __init__(self, head=None):
+class LinkedList:
+    def __init__(self, head=None) -> None:
         self.head = head
-    
-    def search(self, value):
-        """Method to find an element in a Linked List and return the index of the element
-        Args:
-            value (int/str): Element to find
-        Returns:
-            int: -1 if element not found, index value if found (0-n)
-        """
-        if self.head is None:
-            return -1
-        
-        current = self.head
-        idx = 0
-        while current:
-            if current.data == value:
-                print(f"Element {value} found at index {idx}")
-                return idx
-            current = current.next
-            idx += 1
-        print(f"Element {value} not found")
-        return -1
-        
-    
+
     def display(self):
-        self.__display(self.head)
-    
-    def __display(self, head):
-        """Takes the head of a Linked List and displays the whole LinkedList
-        3->5->8->9
-        Args:
-            head (object of class Node): head of a Linked List
-        """
-        if head is None:
-            print("Empty list")
-            return 
-        current = head
-        while current.next:
-            print(current.data, end=" -> ")
+        current = self.head
+        display_string = "Empty List"
+        while current:
+            if current == self.head:
+                display_string = str(current.data)
+            else:
+                display_string = display_string + "->" + str(current.data)
             current = current.next
-        print(current.data, "-> None")
-    
-    def create(self, mode, **kwargs):
-        if mode == 'list':
-            self.__create_from_list(kwargs['node_list'])
-        elif mode == 'cli':
-            self.__create_from_cli()
-        else:
-            raise ValueError("Please enter a valid mode - 'list' or 'cli'")
-        
-    def __create_from_list(self, node_list):
-        for node_value in node_list:
-            self.insert(node_value, mode='head')
+        print(display_string)
+ 
+    def __str__(self) -> str:
+        current = self.head
+        display_string = "Empty List"
+        while current:
+            if current == self.head:
+                display_string = str(current.data)
+            else:
+                display_string = display_string + "->" + str(current.data)
+            current = current.next
+        return display_string
 
-    def __create_from_cli(self):
-        user_input = input("Enter the values of nodes separated by space: ")
-        node_list = user_input.strip(" ").split(" ")
-        for node_value in node_list:
-            self.insert(node_value, mode='head')
+    def insert(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
 
-    def insert(self, data, mode='head', **kwargs):
-        if mode == 'head':
-            self.head = self.__insert_helper(head=self.head, data=data, index=0)
-        elif mode == 'tail':
-            current_count = self.count(mode='iterative')
-            self.head = self.__insert_helper(head=self.head, data=data, index=current_count)
-        elif mode == 'index':
-            self.head = self.__insert_helper(head=self.head, data=data, index=kwargs['index'])
-        else:
-            raise ValueError("Invalid mode - head/tail/index")
-        return self.head
+        new_node.next = self.head
+        self.head = new_node
+        return
 
-    def count(self, mode='iterative'):
-        if mode == 'iterative':
-            count = self.__iterative_count(head=self.head)
-        elif mode == 'recursive':
-            count = self.__recursive_count(head=self.head)
-        else:
-            raise ValueError("Enter a valid mode - iterative or recursive")
+    def delete(self):
+        if self.head is None:
+            return
+        deleted_node = self.head
+        self.head = deleted_node.next
+        return deleted_node
+
+    def search(self, data):
+        current = self.head
+        index = 0
+        while current:
+            if current.data == data:
+                return index
+            current = current.next
+            index += 1
+        return -1
+
+    def size(self):
+        count = 0
+        current = self.head
+        while current:
+            count += 1
+            current = current.next
         return count
     
-    def delete(self, mode='value', **kwargs):
-        """Delete an element by value or by index and return the head of the resulting linked list
-        Args:
-            mode (int/str): 'value' or 'index'
-        """
-        if mode == 'value':
-            deleted_node = self.__delete_by_value(value=kwargs['value'])
-        elif mode == 'index':
-            deleted_node = self.__delete_by_index(index=kwargs['index'])
-        else:
-            raise ValueError("Invalid mode - value/index")
-        return deleted_node
-
-    def __delete_by_value(self, value):
-        if self.head is None:
-            raise IndexError("No head, index out of range")
-        current = self.head
-        trailing_node = None
-        while current:
-            if current.data == value:
-                # Adjust pointers
-                trailing_node.next = current.next
-                deleted_node = current
-                current = None
-                return deleted_node
-            trailing_node = current
-            current = current.next
-        return None
-    
-    def __delete_by_index(self, index):
-        if self.head is None:
-            raise IndexError("No head, index out of range")
-
-        current = self.head
-        idx_current = 0
-        trailing_node = None
-        if index > self.count():
-            raise IndexError("Index out of range")
-        while idx_current < index and current:
-            trailing_node = current
-            current = current.next
-            idx_current += 1
-        trailing_node.next = current.next
-        deleted_node = current
-        return deleted_node
-
-    def __iterative_count(self, head):
-        if head is None:
-            return 0
-        count = 0
-        current_node = head
-        while current_node.next:
-            count += 1
-            current_node = current_node.next
-        return count+1
-    
-    def __recursive_count(self, head):
-        if head is None:
-            return 0
-        else:
-            current = head
-            return 1 + self.__recursive_count(current.next)
-
-    def __insert_helper(self, head, data, index):
-        """Inserts a new Node at the specified index (0-n) of 
-        a Linked List that begins with the node 'head'
-        0 = First node
-        n = Last node
-        Args:
-            data (_type_): Can be any type
-            index (int): Target index of the new Node
-        """
-        new_node = Node(data)
-        
-        # If its an empty list, ignore the index, put the new node as head
-        if head is None:
-            head = new_node
-            return head
-
-        # If list is empty is None and index = 0, insert at head
-        if index == 0:
-            new_node.next = head
-            head = new_node
-            return head
-
-        # If list is empty is None and index is > 0, 
-        # TODO - exception for index < 0
-        current = head
-        current_pointer = 0
-        trailing_node = None
-        while current_pointer < index:
-            trailing_node = current
-            current = current.next
-            current_pointer += 1
-        
-        # Adjust pointers
-        new_node.next = trailing_node.next
-        trailing_node.next = new_node
-        return head
-
-
 if __name__ == "__main__":
-    # l1 = LinkedList()
-    # l1.display()
-    # l1.insert(data=2)
-    # l1.display()
-    # l1.insert(data=4)
-    # l1.insert(data=6)
-    # l1.display()
-    # l1.insert(mode='tail', data=3)
-    # l1.display()
-    # l1.insert(mode='index', data=9, index=2)
-    # l1.display()
-    # l1.search(2)
-    # l1.search(99)
-    # del_node = l1.delete(mode='value', value=9)
-    # print(f"Delete node with data = {del_node.data}")
-    # print(l1.count(mode='iterative'))
-
-    node1 = Node(1)
-    node2 = Node(2)
-    node3 = Node(3)
-    node1.next = node2
-    node2.next = node3
-    ll = LinkedList(node1)
-    ll.search(3)
-
-    l4 = LinkedList()
-    l4.create(mode='list', node_list=[3,6,8])
-    l4.display()
-
-    l5 = LinkedList()
-    l5.create(mode='cli')
-    l5.display()
+    llist = LinkedList()
+    llist.insert(2)
+    llist.insert(3)
+    llist.insert(4)
+    llist.display()
+    llist.delete()
+    llist.display()
+    print(llist)
--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -1,5 +1,3 @@
-from multiprocessing import Value
-from unittest.mock import patch
 import pytest
 from datastructures.linked_list import Node, LinkedList
 
@@ -32,100 +30,44 @@ class TestLinkedList:
     def list_input(self):
         return [1,2,3,4]
 
-    def test_linked_list_creation(self, linked_list_without_head):
-        ll_without_head = linked_list_without_head
-        assert ll_without_head.head is None
-        ll_with_head = LinkedList(Node(1))
-        assert ll_with_head.head is not None
-        assert ll_with_head.head.data == 1
+    def test_insert(self, linked_list_without_head, linked_list_with_head):
+        llnh = linked_list_without_head
+        llnh.insert(2)
+        llnh.insert(1)
+        assert llnh.head.data == 1
+
+        llh = linked_list_with_head
+        assert llh.head.data == 1
+
 
     def test_search(self, linked_list_with_head, linked_list_without_head):
-        linked_list = linked_list_with_head
-        assert linked_list.search(1) == 0
-        assert linked_list.search(2) == 1
-        assert linked_list.search(3) == 2
-        assert linked_list.search(4) == -1
+        llh = linked_list_with_head
+        assert llh.search(1) == 0
+        assert llh.search(2) == 1
+        assert llh.search(3) == 2
+        assert llh.search(4) == -1
 
         ll2 = linked_list_without_head
         assert ll2.search(3) == -1
 
-    def test_display(self, capsys, linked_list_with_head, linked_list_without_head):
-        linked_list_with_head.display()
-        captured = capsys.readouterr()
-        assert captured.out == "1 -> 2 -> 3 -> None\n"
+    def test_delete(self, linked_list_without_head, linked_list_with_head):
+        llnh = linked_list_without_head
+        assert llnh.delete() is None
 
-        linked_list_without_head.display()
-        captured = capsys.readouterr()
-        assert captured.out == "Empty list\n"
+        llh = linked_list_with_head
+        del_node = llh.delete()
+        assert del_node.data == 1
+        assert llh.head.data == 2
+        assert llh.size() == 2
 
-    def test_insert_head(self, linked_list_with_head, linked_list_without_head):
-        linked_list_with_head.insert(10, mode='head')
-        assert linked_list_with_head.head.data == 10
-        
-        linked_list_without_head.insert(1, mode='head')
-        assert linked_list_without_head.head.data == 1
+    # def test_display(self, capsys, linked_list_with_head, linked_list_without_head):
+    #     linked_list_with_head.display()
+    #     captured = capsys.readouterr()
+    #     assert captured.out == "1 -> 2 -> 3 -> None\n"
 
-    def test_insert_tail(self, linked_list_with_head, linked_list_without_head):
-        linked_list_with_head.insert(4, mode='tail')
-        assert linked_list_with_head.head.next.next.next.data == 4
-
-        linked_list_without_head.insert(1, mode='tail')
-        assert linked_list_without_head.head.data == 1
-
-    def test_insert_index(self, linked_list_with_head, linked_list_without_head):
-        linked_list_with_head.insert(5, mode='index', index=1)
-        assert linked_list_with_head.head.next.data == 5
-
-        linked_list_without_head.insert(1, mode='index', index=1)
-        assert linked_list_without_head.head.data == 1
-
-    def test_count_iterative(self, linked_list_with_head, linked_list_without_head):
-        assert linked_list_with_head.count(mode='iterative') == 3
-        assert linked_list_without_head.count(mode='iterative') == 0
-
-    def test_count_recursive(self, linked_list_with_head, linked_list_without_head):
-        assert linked_list_with_head.count(mode='recursive') == 3
-        assert linked_list_without_head.count(mode='recursive') == 0
-
-    def test_delete_value(self, linked_list_with_head, linked_list_without_head):
-        linked_list_with_head.delete(mode='value', value=2)
-        assert linked_list_with_head.search(2) == -1
-
-        with pytest.raises(IndexError):
-            linked_list_without_head.delete(mode='value', value=1)
-
-    def test_delete_index(self, linked_list_with_head, linked_list_without_head):
-        linked_list_with_head.delete(mode='index', index=1)
-        assert linked_list_with_head.search(2) == -1
-        with pytest.raises(IndexError):
-            linked_list_with_head.delete(mode='index', index = 3)
-        with pytest.raises(IndexError):
-            linked_list_without_head.delete(mode='index', index=0)
-
-
-    def test_create_from_list(self, list_input):
-        ll = LinkedList()
-        ll.create(mode='list', node_list=list_input)
-        assert ll.head.data == 4
-        assert ll.head.next.data == 3
-        assert ll.head.next.next.data == 2
-        assert ll.head.next.next.next.data == 1
-        assert ll.head.next.next.next.next is None
-
-    def test_create_from_cli(self):
-        user_input = "3 4 5 "
-        with patch('builtins.input', return_value=user_input):
-            ll = LinkedList()
-            ll.create(mode='cli')
-        assert ll.head.data == '5'
-        assert ll.head.next.data == '4'
-        assert ll.head.next.next.data == '3'
-        assert ll.head.next.next.next is None
-
-    def test_create_mode(self):
-        ll = LinkedList()
-        with pytest.raises(ValueError):
-            ll.create(mode='random')
+    #     linked_list_without_head.display()
+    #     captured = capsys.readouterr()
+    #     assert captured.out == "Empty list\n"
 
 if __name__ == "__main__":
     pytest.main()
