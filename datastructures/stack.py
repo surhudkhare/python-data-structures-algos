8d563b115f664a30a24309b8806657bbd29ce0ea
Surhud <surhudk@gmail.com>
2024-07-19 16:20:49 -0700

    Implement stack 3 ways

--- datastructures/stack.py
+++ datastructures/stack.py
@@ -1,4 +1,7 @@
 from abc import ABC, abstractmethod
+from collections import deque
+from datastructures.linked_list import LinkedList
+
 class Stack(ABC):
     """A linear data structure which follows LIFO
     Can be implemented using 1) in-built list, 2) deque from collections, 3) Linked List
@@ -30,7 +33,7 @@ class StackList(Stack):
         self.stack.append(data)
 
     def pop(self):
-        self.stack.pop()
+        return self.stack.pop()
 
     def peek(self):
         return self.stack[-1]
@@ -40,14 +43,49 @@ class StackList(Stack):
     
     def __str__(self) -> str:
         return str(self.stack)
+
+class StackDeque(Stack):
+    def __init__(self):
+        self.stack = deque()
+
+    def push(self, data):
+        self.stack.append(data)
+
+    def pop(self):
+        return self.stack.pop()
+
+    def peek(self):
+        return self.stack[-1]
+
+    def is_empty(self):
+        return len(self.stack) == 0
     
-s = StackList()
-s.push(3)
-print(s.peek())
-print(s)
-s.push(4)
-print(s.peek())
-print(s)
-s.pop()
-print(s.peek())
-print(s)
+    def __str__(self) -> str:
+        return str(self.stack)    
+
+
+class StackLinkedList(Stack):
+    def __init__(self) -> None:
+        self.stack = LinkedList()
+
+    def push(self, data):
+        self.stack.insert(data)
+
+    def pop(self):
+        pop_element = self.stack.delete()
+        return pop_element.data
+
+    def peek(self):
+        return self.stack.head.data
+
+    def is_empty(self):
+        return self.stack.size() == 0
+    
+    def __str__(self) -> str:
+        return str(self.stack)
+
+if __name__ == "__main__":
+    sll = StackLinkedList()
+    sll.push(3)
+    sll.push(4)
+    print(sll)
--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,4 +1,4 @@
-from datastructures.stack import StackList
+from datastructures.stack import StackList, StackDeque, StackLinkedList
 import pytest
 
 class TestStackList:
@@ -20,7 +20,7 @@ class TestStackList:
         stack = StackList()
         stack.push(1)
         stack.push(2)
-        stack.pop()
+        assert stack.pop() == 2
         assert str(stack) == "[1]"
         stack.pop()
         assert stack.is_empty() is True
@@ -51,6 +51,109 @@ class TestStackList:
         assert str(stack) == "[1, 2]"
         stack.pop()
         assert str(stack) == "[1]"
+
+class TestStackDeque:
+
+    def test_initialization(self):
+        stack = StackDeque()
+        assert stack.is_empty()is True
+        assert str(stack) == "deque([])"
+
+    def test_push(self):
+        stack = StackDeque()
+        stack.push(1)
+        assert stack.is_empty() is False
+        assert str(stack) == "deque([1])"
+        stack.push(2)
+        assert str(stack) == "deque([1, 2])"
+
+    def test_pop(self):
+        stack = StackDeque()
+        stack.push(1)
+        stack.push(2)
+        assert stack.pop() == 2
+        assert str(stack) == "deque([1])"
+        stack.pop()
+        assert stack.is_empty() is True
+
+    def test_peek(self):
+        stack = StackDeque()
+        stack.push(1)
+        assert stack.peek() == 1
+        stack.push(2)
+        assert stack.peek() == 2
+        stack.pop()
+        assert stack.peek() == 1
+
+    def test_is_empty(self):
+        stack = StackDeque()
+        assert stack.is_empty() is True
+        stack.push(1)
+        assert stack.is_empty() is False
+        stack.pop()
+        assert stack.is_empty() is True
+
+    def test_str(self):
+        stack = StackDeque()
+        assert str(stack) == "deque([])"
+        stack.push(1)
+        assert str(stack) == "deque([1])"
+        stack.push(2)
+        assert str(stack) == "deque([1, 2])"
+        stack.pop()
+        assert str(stack) == "deque([1])"
+
+
+# Test cases for StackLinkedList class
+class TestStackLinkedList:
+
+    def test_initialization(self):
+        stack = StackLinkedList()
+        assert stack.is_empty() is True
+
+    def test_push(self):
+        stack = StackLinkedList()
+        stack.push(1)
+        assert stack.is_empty() is False
+        assert str(stack) == "1"
+        stack.push(2)
+        assert str(stack) == "2->1"
+
+    def test_pop(self):
+        stack = StackLinkedList()
+        stack.push(1)
+        stack.push(2)
+        assert stack.pop() == 2
+        assert str(stack) == "1"
+        assert stack.pop() == 1
+        assert stack.is_empty() is True
+
+    def test_peek(self):
+        stack = StackLinkedList()
+        stack.push(1)
+        assert stack.peek() == 1
+        stack.push(2)
+        assert stack.peek() == 2
+        stack.pop()
+        assert stack.peek() == 1
+
+    def test_is_empty(self):
+        stack = StackLinkedList()
+        assert stack.is_empty() is True
+        stack.push(1)
+        assert stack.is_empty() is False
+        stack.pop()
+        assert stack.is_empty() is True
+
+    def test_str(self):
+        stack = StackLinkedList()
+        assert str(stack) == 'Empty List'
+        stack.push(1)
+        assert str(stack) == "1"
+        stack.push(2)
+        assert str(stack) == "2->1"
+        stack.pop()
+        assert str(stack) == "1"
 
 if __name__ == "__main__":
     pytest.main()
